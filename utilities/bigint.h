464584821a7e7b03afb16c84f2140eabb32c47be
opendev0 <opendev0@gmail.com>
2014-09-03 16:48:08 +0200

    A few optimizations and fixes for BigInt type.

--- utilities/bigint.h
+++ utilities/bigint.h
@@ -14,57 +14,73 @@ class BigInt
 {
 public:
     BigInt(){}
-	
+
 	BigInt(BigInt const &bi)
 	{
 		this->_value = bi._value;
 	}
-	
+
     BigInt(uint number)
 	{
 		if (number == 0) this->_value.push_back('0');
-		
+
 		while (number) {
 			this->_value.push_back((number % 10) + '0');
 			number /= 10;
 		}
 	}
-	
-    BigInt(uchar const *number)
+
+    BigInt(char const *number)
 	{
-		uchar const *tmp = number;
-	   
+		char const *tmp = number;
+
 		while (*tmp != 0) {
 			++tmp;
 		}
-	   
+
 		while (tmp > number) {
 			this->_value.push_back(*(--tmp));
 		}
 	}
-	
-    BigInt &operator=(BigInt const &bi)
+
+    BigInt &operator=(BigInt const &rhs)
 	{
-		this->_value = bi._value;
-		
+		this->_value = rhs._value;
+
 		return *this;
 	}
 	
-	BigInt &operator+=(BigInt const &bi)
+	/* ################ ARITHMETIC OPERATORS ################ */
+
+	BigInt operator+(BigInt const &rhs) const
+	{
+		BigInt tmp = *this;
+
+		return (tmp += rhs);
+	}
+	
+	BigInt operator+(uint const &rhs) const
+	{
+		BigInt tmp = *this;
+
+		return (tmp += rhs);
+	}
+	
+	BigInt &operator+=(BigInt const &rhs)
 	{
 		uchar off = 0;	// overflow-flag
 		uchar digit = 0;
 		uint op1Size = this->_value.size();
-		uint op2Size = bi._value.size();
+		uint op2Size = rhs._value.size();
 
 		for (uint i = 0; off || i < op1Size || i < op2Size; ++i) {
 			if (op1Size == i) {
 				this->_value.resize(++op1Size, '0');
 			}
-		   
+
 			digit = this->_value[i] + off;
 			if (op2Size > i) {
-				digit += (bi._value[i] - '0');
+				digit += (rhs._value[i] - '0');
 			}
 
 			if (digit > BigInt::nine) {
@@ -79,20 +95,20 @@ public:
 
 		return *this;
 	}
-	
-    BigInt &operator+=(uint number)
+
+    BigInt &operator+=(uint rhs)
 	{
 		uchar off = 0;	// overflow-flag
 		uchar digit = 0;
 		uint size = this->_value.size();
 
-		for (uint i = 0; off || number || i < size; ++i) {
+		for (uint i = 0; off || rhs || i < size; ++i) {
 			if (size == i) {
 				this->_value.resize(++size, '0');
 			}
 
-			digit = this->_value[i] + (number % 10) + off;
-			number /= 10;
+			digit = this->_value[i] + (rhs % 10) + off;
+			rhs /= 10;
 
 			if (digit > BigInt::nine) {
 				off = 1;
@@ -107,30 +123,36 @@ public:
 		return *this;
 	}
 	
-	BigInt operator+(BigInt const &bi) const
+	inline BigInt &operator++()
+	{
+		return (*this += 1);
+	}
+
+	inline BigInt operator++(int)
 	{
 		BigInt tmp = *this;
-		
-		return (tmp += bi);
+		++(*this);
+
+		return tmp;
 	}
 	
-	BigInt &operator*=(BigInt const &bi)
+	BigInt operator*(BigInt const &rhs) const
 	{
-		vector<uchar> const *small = ((this->numDigits() <= bi.numDigits()) ? &this->_value : &bi._value);
-		vector<uchar> const *big = ((this->numDigits() > bi.numDigits()) ? &this->_value : &bi._value);
+		BigInt result = 0U;
+		vector<uchar> const *small = ((this->numDigits() <= rhs.numDigits()) ? &this->_value : &rhs._value);
+		vector<uchar> const *big = ((this->numDigits() > rhs.numDigits()) ? &this->_value : &rhs._value);
 		uint smallSize = small->size();
 		uint bigSize = big->size();
 		uchar overflow;
 
-		vector<BigInt> values;
-
 		for (uint i = 0; i < smallSize; ++i) {
 			BigInt tmp;
-			tmp._value.resize(i, '0');    // push zeros
+			tmp._value.resize(i, '0');
 			overflow = 0;
-		   
+
 			for (uint j = 0; j < bigSize; ++j) {
-				uint digit = static_cast<uchar>((small->operator[](i) - '0') * (big->operator[](j) - '0') + overflow);
+				// Can have a maximum value of 89 (9*9+8)
+				uchar digit = ((*small)[i] - '0') * ((*big)[j] - '0') + overflow;
 
 				if (digit > 9) {
 					overflow = (digit / 10);
@@ -138,104 +160,122 @@ public:
 				} else {
 					tmp._value.push_back(digit + '0');
 					overflow = 0;
-				}           
+				}
 			}
-		   
+
 			if (overflow > 0) {
 				tmp._value.push_back(overflow + '0');
 			}
-		   
-			values.push_back(tmp);
+
+			result += tmp;
 		}
 
-		this->_value.clear();
+		return result;
+	}
 
-		for (uint i = 0; i < values.size(); ++i) {
-			*this += values[i];
-		}
+	inline BigInt &operator*=(BigInt const &rhs)
+	{
+		*this = *this * rhs;
 
 		return *this;
 	}
-	
-    BigInt &operator*=(uint number)
+
+    BigInt &operator*=(uint const rhs)
 	{
 		BigInt tmp = *this;
-		--number;
 
-		for (uint i = 0; i < number; ++i) {
+		for (uint i = 1; i < rhs; ++i) {
 			*this += tmp;
 		}
 
 		return *this;
 	}
 	
-	BigInt &operator*(BigInt const &rhs) const
-	{
-		BigInt tmp = *this;
-		
-		return (tmp *= rhs);
-	}
+	/* ######################## COMPARISON OPERATORS ################ */
 	
-    inline BigInt &operator++()
+	inline bool operator==(BigInt const &rhs) const
 	{
-		return (*this += 1);
+		return (this->_value == rhs._value);
 	}
-	
-	inline BigInt operator++(int)
+
+	inline bool operator==(uint const rhs) const
 	{
-		BigInt tmp = *this;
-		++(*this);
-		
-		return tmp;
+		return (BigInt(rhs)._value == this->_value);
 	}
-	
-    bool operator<(BigInt const &bi) const
+
+	inline bool operator!=(BigInt const &rhs) const
 	{
-		uint op1Size = this->_value.size();
-		uint op2Size = bi._value.size();
+		return !(*this == rhs);
+	}
+
+	inline bool operator!=(uint const rhs) const
+	{
+		return !(*this == rhs);
+	}
+
+    bool operator<(BigInt const &rhs) const
+	{
+		size_t op1Size = this->numDigits();
+		size_t op2Size = rhs.numDigits();
 
 		if (op1Size < op2Size) {
 			return true;
 		} else if (op1Size > op2Size) {
 			return false;
 		} else {
-			for (uint i = (op1Size - 1); i; --i) {
-				if (this->_value[i] < bi._value[i]) {
+			do {
+				--op1Size;
+				
+				if (this->_value[op1Size] < rhs._value[op1Size]) {
 					return true;
 				} else {
 					return false;
 				}
-			}
+			} while (op1Size);
 
 			return false;
 		}
 	}
 	
-	inline bool operator==(BigInt const &rhs) const
+	bool operator>(BigInt const &rhs) const
 	{
-		return (this->_value == rhs._value);
+		size_t op1Size = this->numDigits();
+		size_t op2Size = rhs.numDigits();
+
+		if (op1Size > op2Size) {
+			return true;
+		} else if (op1Size < op2Size) {
+			return false;
+		} else {
+			do {
+				--op1Size;
+				
+				if (this->_value[op1Size] > rhs._value[op1Size]) {
+					return true;
+				} else {
+					return false;
+				}
+			} while (op1Size);
+
+			return false;
+		}
 	}
 	
-	inline bool operator==(uint const rhs) const
+	inline bool operator>=(BigInt const &rhs)
 	{
-		return (BigInt(rhs)._value == this->_value);
+		return !(*this < rhs);
 	}
 	
-	inline bool operator!=(BigInt const &rhs) const
+	inline bool operator<=(BigInt const &rhs)
 	{
-		return !(*this == rhs);
+		return !(*this > rhs);
 	}
-	
-	inline bool operator!=(uint const rhs) const
-	{
-		return !(*this == rhs);
-	}
-	
+
 	inline uint operator[](size_t const i) const
 	{
 		return (this->_value[this->numDigits() - (1 + i)] - '0');
 	}
-	
+
 	/**
 	 * @note Changes the object itself rather than returning a new one
 	 */
@@ -246,7 +286,7 @@ public:
 			this->_value.push_back('1');
 		} else if (power > 1) {
 			BigInt tmp(*this);
-			
+
 			for (uint i = 1; i < power; ++i) {
 				*this *= tmp;
 			}
@@ -254,12 +294,12 @@ public:
 
 		return *this;
 	}
-	
+
 	inline uint numDigits() const
 	{
 		return this->_value.size();
 	}
-	
+
     uint digitSum() const
 	{
 		uint size = this->_value.size();
@@ -274,12 +314,12 @@ public:
 
     friend ostream &operator<<(ostream &os, BigInt const &bi)
 	{
-		unsigned int i = bi._value.size();
-		
+		unsigned int i = bi.numDigits();
+
 		do {
 			os << bi._value[--i];
 		} while (i);
-		
+
 		return os;
 	}
 
